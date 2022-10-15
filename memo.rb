8cf25bfae5afc4b0691c0b3060bbf4bc4fa8342e
maksimdotcom <ya.maksim0511@gmail.com>
2022-10-15 19:58:49 +0300

    notepad v.1, working

--- memo.rb
+++ memo.rb
@@ -1,10 +1,22 @@
 class Memo < Post
 
   def read_from_console
+    puts "Новая строка до \"end\":"
 
+    @text = []
+    line = nil
+
+    while line != "end" do
+      line = STDIN.gets.chomp
+      @text << line
+    end
+
+    @text.pop
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} "
 
+    return @text.unshift(time_string)
   end
 end
--- task.rb
+++ task.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Task < Post
   def initialize
     super
@@ -6,10 +8,20 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
 
+    puts "До какого числа ДД.ММ.ГГГГ, например 11.05.2000"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")}"
 
+    deadline = "Крайний срок: #{@due_date}"
+
+    return [deadline, @text, time_string]
   end
 end
